0c3208eab4e64ee2d04a78830a251a7b3a2d031e
HajiMuhammadHafidzuddinHajiUmar <blu1mars@gmail.com>
2023-05-24 15:38:33 +0800

    feat: completed CRUD | refactor: README

--- app.js
+++ app.js
@@ -42,6 +42,25 @@ class UI {
         List.appendChild(row);
     }
 
+    static deleteBook(el) {
+        if (el.classList.contains("delete")) {
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, className) {
+        // Make div
+        const div = document.createElement("div");
+        div.className = `container alert alert-${className}`;
+        div.innerText = message;
+
+        // Add div to DOM
+        const form = document.querySelector("#book-form");
+        form.insertAdjacentElement("beforebegin", div);
+
+        setTimeout(() => document.querySelector(".alert").remove(), 3000);
+    }
+
     static clearFields() {
         document.querySelector("#title").value = "";
         document.querySelector("#author").value = "";
@@ -57,6 +76,7 @@ document.addEventListener("DOMContentLoaded", UI.displayBooks())
 // Event: Add a Book
 const form = document.querySelector("#book-form");
 form.addEventListener("submit", submitNewBook);
+
 function submitNewBook(e) {
     e.preventDefault();
 
@@ -65,38 +85,23 @@ function submitNewBook(e) {
     const author = document.querySelector("#author").value;
     const isbn = document.querySelector("#isbn").value;
 
-    const book = new Book(title, author, isbn);
+    // Validation
+    if (title === "" || author === "" || isbn === "") {
+        UI.showAlert("Please fill all fields", "danger");
+    } else {
+        const book = new Book(title, author, isbn);
 
-    UI.addBookToList(book);
+        UI.addBookToList(book);
 
-    UI.clearFields();
+        UI.showAlert("Successfully added new book", "success");
+
+        UI.clearFields();
+    }
 }
 
-// Event: Remove a Book
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-Step 1: Book, UI, UI.displayBooks, UI.addBookToList
-Step 2: Add a book
-*/
+// Event: Remove a Book - since there is multiple delete btn, use event propogation ie. select parent of element to be deleted
+const bookList = document.querySelector("#book-list");
+bookList.addEventListener("click", e => {
+    UI.deleteBook(e.target);
+    UI.showAlert("Book removed", "success");
+})
